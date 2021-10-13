16cd7a3903eefa47a29cf567bccbc9a1b951f23b
zhelyazkokichukov <kichukovzh@gmail.com>
2021-10-13 11:23:08 +0300

    Added update and delete functionality for the tvshows

--- MyCustomIMDb/Controllers/TVShowsController.cs
+++ MyCustomIMDb/Controllers/TVShowsController.cs
@@ -61,5 +61,69 @@ namespace MyCustomIMDb.Controllers
 
             return View(all);
         }
+
+        public IActionResult Details(int id)
+        {
+            var tvshow = data.TVShows
+                .Where(t => t.Id == id)
+                .Select(t => new TVShowDetails
+                {
+                    Id = t.Id,
+                    Title = t.Title,
+                    Length = t.Length,
+                    PlotSummary = t.PlotSummary,
+                    ImageURL = t.ImageUrl,
+                    ReleaseDate = t.ReleaseDate,
+                    Storyline = t.Storyline
+                })
+                .FirstOrDefault();
+
+            //I Should add TVShow rating table and ratings column
+
+            return View(tvshow);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var tvshow = data.TVShows.Where(t => t.Id == id)
+                .FirstOrDefault();
+
+            data.TVShows.Remove(tvshow);
+            data.SaveChanges();
+
+            return View(tvshow);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var tvshow = data.TVShows.Where(t => t.Id == id).FirstOrDefault();
+
+            return View(tvshow);
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(TVShow preEditedTVShow)
+        {
+            var postEditedTVShow = new TVShow
+            {
+                Title = preEditedTVShow.Title,
+                PlotSummary = preEditedTVShow.PlotSummary,
+                ImageUrl = preEditedTVShow.ImageUrl,
+                Length = preEditedTVShow.Length,
+                ReleaseDate = preEditedTVShow.ReleaseDate,
+                Storyline = preEditedTVShow.Storyline
+            };
+
+            data.TVShows.Remove(preEditedTVShow);
+
+            data.TVShows.Update(postEditedTVShow);
+
+            data.SaveChanges();
+
+            return Redirect("/TVShows/All");
+
+        }
     }
 }
---
+++ MyCustomIMDb/Models/TVShow/TVShowDetails.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCustomIMDb.Models.TVShow
+{
+    public class TVShowDetails
+    {
+    
+        public int Id { get; init; }
+
+        public string Title { get; init; }
+
+        public string PlotSummary { get; init; }
+
+        public string Storyline { get; init; }
+
+        public string Length { get; init; }
+
+        public string ImageURL { get; init; }
+
+        public string ReleaseDate { get; init; }
+
+        public int Rate { get; init; }
+
+    }
+}
